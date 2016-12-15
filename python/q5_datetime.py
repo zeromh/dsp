da5b6589ea7e3d9d953090ee0234a0aff2231c8f
zeromh <zeromh@users.noreply.github.com>
2016-12-15 16:11:45 -0500

    Answer Q5

--- python/q5_datetime.py
+++ python/q5_datetime.py
@@ -1,13 +1,32 @@
 # Hint:  use Google to find python function
+import datetime
 
-####a) 
-date_start = '01-02-2013'  
-date_stop = '07-28-2015'   
+def print_date_diff(date_start, date_stop, format):
+  '''
+  Prints the difference (in days) between two date strings in a given format.
+  '''
+  date_start = datetime.datetime.strptime(date_start, format)
+  date_stop = datetime.datetime.strptime(date_stop, format)
+  print 'Days between %s and %s:' % \
+      (date_start.strftime('%m-%d-%Y'), date_stop.strftime('%m-%d-%Y')), \
+      (date_stop - date_start).days
 
-####b)  
-date_start = '12312013'  
-date_stop = '05282015'  
+if __name__ == '__main__':
+  ####a) 
+  date_start = '01-02-2013'  
+  date_stop = '07-28-2015'   
 
-####c)  
-date_start = '15-Jan-1994'  
-date_stop = '14-Jul-2015'  
+  print_date_diff(date_start, date_stop, '%m-%d-%Y')
+
+
+  ####b)  
+  date_start = '12312013'  
+  date_stop = '05282015'  
+
+  print_date_diff(date_start, date_stop, '%m%d%Y')
+
+  ####c)  
+  date_start = '15-Jan-1994'  
+  date_stop = '14-Jul-2015'  
+
+  print_date_diff(date_start, date_stop, '%d-%b-%Y')
